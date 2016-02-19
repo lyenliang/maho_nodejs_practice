56d6328ea7463ed15d05c4ee3b21ded0777f0123
lyenliang <i745m@hotmail.com>
2016-02-19 10:24:35 +0800

    Refactor

--- app/models/dbManager.js
+++ app/models/dbManager.js
@@ -1,24 +1,11 @@
-exports.checkExistence = function(res, con, guid) {
-    return function(err, rows) {
+exports.isNewGuid = function(res, con, guid, newPlayerCallback, oldPlayerCallback) {
+    con.query('SELECT guid FROM maho_game.players WHERE guid = ?', guid, function(err, rows) {
         if (err) throw err;
         // this guid hasn't been registered
         if (rows.length == 0) {
-            console.log('new guid: ' + guid);
-            //  log time
-            con.query('INSERT INTO maho_log.character_login SET ?', loginTimeStamp, function (err, res) {
-                if (err) throw err;
-                console.log('Last insert ID:', res.insertId);
-            });
-            con.query('INSERT INTO maho_game.players SET ?', player, function (err, result) {
-                if (err) throw err;
-            });
-            res.send('score: ' + player.score);
+            newPlayerCallback(res, con, guid);
         } else {
-            console.log('old player: ' + guid);
-            con.query('SELECT score from maho_game.players WHERE guid = ?', guid, function (err, rows) {
-                if (err) throw err;
-                res.send('score: ' + rows[0].score);
-            });
+            oldPlayerCallback(res, con, guid);
         }
-    }
+    });
 };
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,15 @@
+var dbManager = require('./models/dbManager.js');
+
 module.exports = function(app, passport, con) {
+
     app.get('/', function(req, res) {
         res.send('<h1>Hello world</h1>');
+    });
+
+    app.post('/update_score', function(req, res){
+        var guid = req.body.guid;
+        var score = req.body.score;
+
     });
 
     app.post('/login', function(req, res) {
@@ -18,27 +27,26 @@ module.exports = function(app, passport, con) {
             score: 0
         }
         // check if guid has already been registered in DB
-        con.query('SELECT guid FROM maho_game.players WHERE guid = ?', guid, function(err, rows){
-           if(err) throw err;
-            // this guid hasn't been registered
-            if(rows.length == 0) {
-                console.log('new guid: ' + guid);
-                //  log time
-                con.query('INSERT INTO maho_log.character_login SET ?', loginTimeStamp, function(err, res){
-                    if(err) throw err;
-                    console.log('Last insert ID:', res.insertId);
-                });
-                con.query('INSERT INTO maho_game.players SET ?', player, function(err, result){
-                    if(err) throw err;
-                });
-                res.send('score: ' + player.score);
-            } else {
-                console.log('old player: ' + guid);
-                con.query('SELECT score from maho_game.players WHERE guid = ?', guid, function(err, rows) {
-                    if(err) throw err;
-                    res.send('score: ' + rows[0].score);
-                });
-            }
+        dbManager.isNewGuid(res, con, guid,
+            function(res, con, guid) {
+            // new player
+            console.log('new guid: ' + guid);
+            //  log time
+            con.query('INSERT INTO maho_log.character_login SET ?', loginTimeStamp, function (err, res) {
+                if (err) throw err;
+                console.log('Last insert ID:', res.insertId);
+            });
+            con.query('INSERT INTO maho_game.players SET ?', player, function (err, result) {
+                if (err) throw err;
+            });
+            res.send('score: ' + player.score);
+        }, function(res, con, guid) {
+            // old player
+            console.log('old player: ' + guid);
+            con.query('SELECT score from maho_game.players WHERE guid = ?', guid, function (err, rows) {
+                if (err) throw err;
+                res.send('score: ' + rows[0].score);
+            });
         });
     });
 
